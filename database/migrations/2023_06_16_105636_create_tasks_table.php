4890b06ab009919d865d8fcb493af28a679d7762
rashid44 <rashid2k22@gmail.com>
2023-06-17 20:52:06 +0600

    Change description datatype from string to text

--- database/migrations/2023_06_16_105636_create_tasks_table.php
+++ database/migrations/2023_06_16_105636_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->boolean('is_completed')->default(false);
             $table->timestamps();
         });
